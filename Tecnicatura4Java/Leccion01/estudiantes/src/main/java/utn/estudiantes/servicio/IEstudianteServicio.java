78869a9d61c4a256862a0cd4ba038d5c27fc2a9f
Valentin Albornoz <valeg486@gmail.com>
2023-08-30 21:14:22 -0300

    actualizamos el IEstudianteServicio

--- Tecnicatura4Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
+++ Tecnicatura4Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
@@ -3,7 +3,7 @@ package utn.estudiantes.servicio;
 import utn.estudiantes.modelo.Estudiante;
 
 import java.util.List;
-public class IEstudianteServicio {
+public interface IEstudianteServicio {
     public List<Estudiante> listarEstudiantes();
     public Estudiante buscarEstudiantePorId(Integer idEstudiante);
     public void guardarEstudiante(Estudiante estudiante);
