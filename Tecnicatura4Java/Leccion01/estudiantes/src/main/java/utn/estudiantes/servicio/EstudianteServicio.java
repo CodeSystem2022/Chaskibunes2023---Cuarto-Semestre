2e915ae4371ed1cf28140a1daff188198bdbcead
Valentin Albornoz <valeg486@gmail.com>
2023-08-30 21:44:49 -0300

    actualización de los ficheros  por nombres
    actualizamos los ficheros por nombres correspondientes de la bd

--- Tecnicatura4Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Tecnicatura4Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -1,6 +1,6 @@
 package utn.estudiantes.servicio;
 
-import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.modelo.Estudiantes2022;
 
 import java.util.List;
 public class EstudianteServicio implements IEstudianteServicio {
