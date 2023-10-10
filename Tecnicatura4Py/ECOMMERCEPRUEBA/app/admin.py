21de218961720b663bea2826a19d80a83e11bcf2
Gabriel Muñoz Rodriguez <gabriel_13l@hotmail.com>
2023-10-10 10:47:21 -0300

    Modificaciones y pertinentes
    python clase 06

--- Tecnicatura4Py/ECOMMERCEPRUEBA/app/admin.py
+++ Tecnicatura4Py/ECOMMERCEPRUEBA/app/admin.py
@@ -16,22 +16,22 @@ class CustomerModelAdmin(admin.ModelAdmin):
     list_display=['id','user','locality','city','state','zipcode']
 
 # Registra el modelo Cart en el panel de administración.
-@admin.register(Cart)
-class CartModelAdmin(admin.ModelAdmin):
-    list_display=['id','user','products','quantity']
+@admin.register(Cart) 
+class CartModelAdmin(admin.ModelAdmin): 
+    list_display=['id','user','products','quantity'] 
     
-    # Define una función para mostrar enlaces a las páginas de edición de productos en el panel de administración.
-    def products(self, obj):
-        # Genera un enlace a la página de edición del producto usando el ID del producto.
-        link = reverse("admin:app_product_change", args=[obj.product.pk])
-        return format_html('<a href="{}">{}</a>', link, obj.product.title)
+    # Define una función para mostrar enlaces a las páginas de edición de productos en el panel de administración. 
+    def products(self, obj): 
+        # Genera un enlace a la página de edición del producto usando el ID del producto. 
+        link = reverse("admin:app_product_change", args=[obj.product.pk]) 
+        return format_html('<a href="{}">{}</a>', link, obj.product.title) 
 
-# Registra el modelo Payment en el panel de administración.
-@admin.register(Payment)
-class PaymentModelAdmin(admin.ModelAdmin):
-    list_display=['id','user','amount','razorpay_order_id','razorpay_payment_status','razorpay_payment_id','paid']
+# Registra el modelo Payment en el panel de administración. 
+@admin.register(Payment) 
+class PaymentModelAdmin(admin.ModelAdmin): 
+    list_display=['id','user','amount','razorpay_order_id','razorpay_payment_status','razorpay_payment_id','paid'] 
 
-# Registra el modelo OrderPlaced en el panel de administración
-@admin.register(OrderPlaced)
-class OrderPlacedModelAdmin(admin.ModelAdmin):
-    list_display=['id','user','product','quantity','order_date','status','payment']
+# Registra el modelo OrderPlaced en el panel de administración 
+@admin.register(OrderPlaced) 
+class OrderPlacedModelAdmin(admin.ModelAdmin): 
+    list_display=['id','user','product','quantity','order_date','status','payment'] 
