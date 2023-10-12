412a39b263265ca8be296cb7a496140f9bc97dbf
Ale Maugouber <alemago14@yahoo.com>
2023-10-12 18:16:12 -0300

    Clase 08 Node

--- Tecnicatura4Node/02-Fundamentos_node/async/callbackhell.js
+++ Tecnicatura4Node/02-Fundamentos_node/async/callbackhell.js
@@ -1,33 +1,34 @@
-function hola(nombre, miCallback) {
-  setTimeout(function () {
-    console.log("Hola" + nombre);
-    miCallback(nombre);
-  }, 1000);
-}
-
-function hablar(miCallbackHablar) {
-  setTimeout(function () {
-    console.log("bla bla bla bla");
-    miCallbackHablar();
-  }, 1000);
-}
-
-function adios(nombre, otroCallback) {
-  setTimeout(function () {
-    console.log("Adios " + nombre);
-    otroCallback();
-  }, 1500);
-}
-
-function conversacion(nombre, veces, miCallback) {
-  if (veces > 0) {
-    hablar(function () {
-      conversacion(nombre, --veces, miCallback);
-    });
-  } else {
-    miCallback(nombre, callback);
-  }
-}
+function hola(nombre, miCallback) { 
+  setTimeout(function () { 
+    console.log("Hola" + nombre); 
+    miCallback(nombre); 
+  }, 1000); 
+} 
+ 
+function hablar(miCallbackHablar) { 
+  setTimeout(function () { 
+    console.log("bla bla bla bla"); 
+    miCallbackHablar(); 
+  }, 1000); 
+} 
+ 
+function adios(nombre, otroCallback) { 
+  setTimeout(function () { 
+    console.log("Adios " + nombre); 
+    otroCallback(); 
+  }, 1500); 
+} 
+ 
+function conversacion(nombre, veces, miCallback) { 
+  if (veces > 0) { 
+    hablar(function () { 
+      conversacion(nombre, --veces, miCallback); 
+    }); 
+  } else { 
+    miCallback(nombre, callback); 
+  } 
+} 
+ 
 //--Proceso principal
 console.log("Inciando el proceso... ");
 hola("Ariel", function (nombre) {
