a9c132c26638010d3de07a48e2fb7e731030e5a4
Ale Maugouber <alemago14@yahoo.com>
2023-10-26 18:24:55 -0300

    Clase 11 Java

--- Tecnicatura4Java/ToDo_Front_-_API-main/Back/ToDo API/src/main/java/ar/com/utnfrsr/todoapp/controller/TaskController.java
+++ Tecnicatura4Java/ToDo_Front_-_API-main/Back/ToDo API/src/main/java/ar/com/utnfrsr/todoapp/controller/TaskController.java
@@ -13,31 +13,31 @@ import java.util.List;
 @RestController /*Anotación para confirmar de que se trata de un controlador el cual será revisado por swagger*/
 @RequestMapping("api/v1/tasks")
 @RequiredArgsConstructor
-public class TaskController { //esta capa solo se comunica con la capa de servicio
+public class TaskController { //esta capa solo se comunica con la capa de servicio 
 
     private final TaskService taskService;
 
-    @PostMapping("/create")
+    @PostMapping("/create") 
     public ResponseEntity<TaskResponseDTO> createTask(@RequestBody TaskRequestDTO taskRequestDTO) {
         TaskResponseDTO task = taskService.createTask(taskRequestDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
-    @GetMapping("/all")
+    @GetMapping("/all") 
     public ResponseEntity<List<TaskResponseDTO>> findAll(){
         List<TaskResponseDTO> tasks = taskService.findAll();
         return ResponseEntity.ok(tasks);
     }
 
-    @PatchMapping("/mark_as_finished/{id}/{finished}")
+    @PatchMapping("/mark_as_finished/{id}/{finished}") 
     public ResponseEntity<Void> markAsFinished(@PathVariable("id") Long id, @PathVariable("finished") boolean finished) {
         this.taskService.updateTaskAsFinished(id,finished);
         return ResponseEntity.noContent().build(); //luego de que se ejecute la tarea devuelve un 204 siempre
     }
 
     @DeleteMapping("delete/{id}")
-    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
-        this.taskService.deleteById(id);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) { 
+        this.taskService.deleteById(id); 
+        return ResponseEntity.noContent().build(); 
     }
 }
