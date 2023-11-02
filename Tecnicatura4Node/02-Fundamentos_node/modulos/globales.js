90276f43d38a187382487c885ed1c92923ba8537
Valentin Albornoz <valeg486@gmail.com>
2023-11-01 21:47:46 -0300

    update

--- Tecnicatura4Node/02-Fundamentos_node/modulos/globales.js
+++ Tecnicatura4Node/02-Fundamentos_node/modulos/globales.js
@@ -1,16 +1,38 @@
 // this === global = true
 
 // Mostrar algo en consola
-console.log();
+// console.log();
 
-//Mostrar un mensaje en forma de erro
-console.error();
+// //Mostrar un mensaje en forma de erro
+// console.error();
 
-//Ejecutar un código después de un intervalo de tiempo
-setTimeout(() => {});
+// //Ejecutar un código después de un intervalo de tiempo
+// setTimeout(() => {});
 
-//Ejecutar un código cada intervalo de tiempo
-setInterval(() => {});
+// //Ejecutar un código cada intervalo de tiempo
+// setInterval(() => {});
 
-//Da prioridad de ejecución a una función asincronica
-setImmdiate(() => {});
+// //Da prioridad de ejecución a una función asincronica
+// setImmdiate(() => {});
+
+// console.log(setInterval);
+
+let i = 0;
+let intervalo = setInterval(() => {
+    console.log('Hola');
+    if(i === 3){
+        clearInterval(intervalo); //detenemos la función
+    }
+    i++; 
+}, 1000);
+
+setImmediate(() => {
+    console.log('Saludo inmediato')
+});
+
+// require();
+
+console.log(__filname);
+
+globalThis.miVariable = 'mi variable global';
+console.log(miVariable)
